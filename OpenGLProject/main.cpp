debb7d782a1dbd951a87109da157743e509dc1d3
MikaelMarkussen <244604@stud.inn.no>
2021-11-12 12:30:16 +0100

    .

--- OpenGLProject/main.cpp
+++ OpenGLProject/main.cpp
@@ -4,14 +4,28 @@
 
 int main()
 {
+	//denne initsialiserer GLFW
 	glfwInit();
+
+	//forteller GLFW hvilken versjon av OpenGL
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//forteller GLFW hva slags core profil jeg bruker
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow* window = glfwCreateWindow(480, 480, "Your mom", NULL, NULL);
 
+	//her lager jeg et object av GLFWwindow som er 480 x480 pixler 
+	GLFWwindow* window = glfwCreateWindow(480, 480, "OpenGL", NULL, NULL);
+	//introduserer window in i contexten
 	glfwMakeContextCurrent(window);
+	//laster in GLAD så den configurer OpenGL
+	gladLoadGL();
 
+	glViewport(0,0,480,480);
+
+	glClearColor(0.07f, 0.13f, 0.17f,1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+		 
 	while(!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
